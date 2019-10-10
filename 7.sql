fea2388d9368ce01094b812d30272e41682b968d
Nicholas DeMarco <ndemarco@cs.ualberta.ca>
2019-10-10 15:08:30 -0600

    7 fixed

--- 7.sql
+++ 7.sql
@@ -1,3 +1,3 @@
 .print Question 7 - ndemarco
 
-select v.color, count(t.regno), avg(t.fine) from registrations r, vehicles v left outer join tickets t on t.regno = r.regno where date('now','+1 month') <= date(r.expiry) and r.vin = v.vin group by v.color;
+select v.color, cast(count(t.regno) as float)/cast(count(r.regno) as float), avg(t.fine), max(t.fine) from registrations r, vehicles v left outer join tickets t on t.regno = r.regno where date('now','+1 month') <= date(r.expiry) and r.vin = v.vin group by v.color;
