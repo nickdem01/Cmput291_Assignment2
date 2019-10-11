7fafcdeda1c8f20c4b125766dbfe52f0db4dd442
Nicholas DeMarco <ndemarco@cs.ualberta.ca>
2019-10-11 10:23:53 -0600

    9 update 10

---
+++ 10.sql
@@ -0,0 +1,3 @@
+.print Question 10 - ndemarco
+
+select p.fname, p.lname from personDetails p where p.ticketsRcvd >= 3;
--- 9.sql
+++ 9.sql
@@ -1,4 +1,3 @@
 .print Question 9 - ndemarco
 
-create view personDetails as select p.fname, p.lname, p.bdate, p.bplace, count(t.tno), count(r.regno) from tickets t, registrations r, persons p
-  where r.fname = p.fname and r.lname = p.lname and t.regno = r.regno and t.vdate >= date('now','-1 year') group by p.fname, p.lname, p.bdate, p.bplace;
+create view personDetails(fname,lname,bdate,bplace,ticketsRcvd,carsowned) as select p.fname, p.lname, p.bdate, p.bplace, count(t.tno), count(r.regno) from tickets t, registrations r, persons p where r.fname = p.fname and r.lname = p.lname and t.regno = r.regno and t.vdate >= date('now','-1 year') group by p.fname, p.lname, p.bdate, p.bplace;
