b2ccd1c4dcb108b00f5f13a423be86783a0be747
Nicholas DeMarco <ndemarco@cs.ualberta.ca>
2019-11-02 00:22:21 -0600

    moar nonsense

--- minproj.py
+++ minproj.py
@@ -2,7 +2,7 @@ import sqlite3
 import random
 from date import datetime
 
-def register_birth():
+def register_birth(login):
 
 	bregno = random.randint(100000,999999)
 	
@@ -58,7 +58,7 @@ def register_birth():
 		paAddress = input('Please enter the address of Mother')
 		paPhone = input('Please enter the phone number of Mother')
 		
-		cursor.execute('insert into persons values (?, ?, ?, ?, ?, ?);', (paFname, paLname, pabDate, pabPlace, paAddress, paPhone)
+		cursor.execute('insert into persons values (?, ?, ?, ?, ?, ?);', (paFname, paLname, pabDate, pabPlace, paAddress, paPhone))
 		conn.commit()
 
 	p2Fname = input('What is the first name of the Father')
@@ -79,28 +79,94 @@ def register_birth():
                 paAddress = input('Please enter the address of Father')
                 paPhone = input('Please enter the phone number of Father')
 
-                cursor.execute('insert into persons values (?, ?, ?, ?, ?, ?);', (paFname, paLname, pabDate, pabPlace, paAddress, paPhone)
+                cursor.execute('insert into persons values (?, ?, ?, ?, ?, ?);', (paFname, paLname, pabDate, pabPlace, paAddress, paPhone))
                 conn.commit()
 
-	cursor.execute('insert into births values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);', (bregno, bfname, blname, bregdate, bregplace, bgender, p1Fname, p1Lname, p2Fname, p2Lname) 
+	cursor.execute('insert into births values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);', (bregno, bfname, blname, bregdate, bregplace, bgender, p1Fname, p1Lname, p2Fname, p2Lname)) 
 	conn.commit()
 
 	#create the persons entry
 	
 	nbdate = input('Please enter the birthdate of the baby')
 	
-	cursor.execute('select p.address from persons p where p.fname = ? and p.lname = ?;', (p1Fname, p1Lname)
+	cursor.execute('select p.address from persons p where p.fname = ? and p.lname = ?;', (p1Fname, p1Lname))
 	baddress = cursor.fetchone()
 
 
-	cursor.execute('select p.phone from persons p where p.fname = ? and p.lname = ?;', (p1Fname, p1Lname)
+	cursor.execute('select p.phone from persons p where p.fname = ? and p.lname = ?;', (p1Fname, p1Lname))
 	bphone = cursor.fetchone()
 
-	cursor.execute('insert into persons values (?, ?, ?, ?, ?, ?);', (bfname, blname, nbdate, nbplace, baddress, bphone)
+	cursor.execute('insert into persons values (?, ?, ?, ?, ?, ?);', (bfname, blname, nbdate, nbplace, baddress, bphone))
 	conn.commit
 
+
+def registerMarriage():
+	
+	p1Fname = input('Please enter the first name of Partner 1')
+	p1Lname = input('Please enter the last name of Partner 1')
+	p2Fname = input('Please enter the first name of Partner 2')
+	p2Lname = input('Please enter the last name of Partner 2')
+	
+
+	mregdate = datetime.date(datetime.now())
+
+	mregno = random.randint(100000,999999)
+	
+	row = None
+	cursor.execute('select * from marriages m where m.regno = ?;', (mregno,))
+	row = cursor.fetchone()
+	
+	if row != None:
+		
+		while row != None:
+
+	                mregno = random.randint(100000,999999)
+
+        		row = None
+        		cursor.execute('select * from marriages m where m.regno = ?;', (mregno,))
+        		row = cursor.fetchone()
+
+	row = None 
+	cursor.execute('select * from persons p where p.fname = ? and p.lname = ?;',(p1Fname,), (p1Lname,))
+	row = cursor.fetchone()
+	
+	if row  == None:
+		
+		print('Partner 1 info missing please enter the missing data')
+
+		paFname = input('Please enter the first name of Partner 1')
+		paLname = input('Please enter the last name of Partner 1')
+		pabDate	= input('Please enter the birthdate of Partner 1')
+		pabPlace = input('Please enter the birthplace of Partner 1')
+		paAddress = input('Please enter the address of Partner 1')
+		paPhone = input('Please enter the phone number of Partner 1')
+		
+		cursor.execute('insert into persons values (?, ?, ?, ?, ?, ?);', (paFname, paLname, pabDate, pabPlace, paAddress, paPhone))
+		conn.commit()
+
+	row = None 
+	cursor.execute('select * from persons p where p.fname = ? and p.lname = ?;',(p2Fname,), (p2Lname,))
+	row = cursor.fetchone()
+	
+	if row  == None:
+		
+		print('Partner 2 info missing please enter the missing data')
+
+		paFname = input('Please enter the first name of Partner 2')
+		paLname = input('Please enter the last name of Partner 2')
+		pabDate	= input('Please enter the birthdate of Partner 2')
+		pabPlace = input('Please enter the birthplace of Partner 2')
+		paAddress = input('Please enter the address of Partner 2')
+		paPhone = input('Please enter the phone number of Partner 2')
+		
+		cursor.execute('insert into persons values (?, ?, ?, ?, ?, ?);', (paFname, paLname, pabDate, pabPlace, paAddress, paPhone))
+		conn.commit()
+
+	cursor.execute('inset into marriages values (?, ?, ?, ?, ?, ?, ?);', (mregno, mregdate, mregplace, p1Fname, p1Lname, p2Fname, p2Lname))
+	conn.commit()
 			
- main():
+main():
+
 	
 	#dbpath code needs to be edited to accomadate command line arg database code will not function on your computer as it is now
 	dbpath = "./a2.db"
